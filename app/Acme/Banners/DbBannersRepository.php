b8893d019ff6b5d2835004d99cf8f8a123600e51
Pavel Nosov <soundrussian@gmail.com>
2014-10-24 01:20:23 +0400

    Увеличение количества просмотров баннеров

--- app/Acme/Banners/DbBannersRepository.php
+++ app/Acme/Banners/DbBannersRepository.php
@@ -1,7 +1,11 @@
 <?php namespace Acme\Banners;
 
+use Banner;
+
 class DbBannersRepository implements BannersRepositoryInterface
 {
+
+    protected $banners;
 
     public function __construct(BannerPlaces $places)
     {
@@ -10,7 +14,15 @@ class DbBannersRepository implements BannersRepositoryInterface
 
     public function bannersFor($place)
     {
-        return $this->places->query($place)->orderByRaw('RANDOM()')->get();
+        $this->banners = $this->places->query($place)->orderByRaw('RANDOM()')->get();
+        $this->increaseViews();
+        return $this->banners;
+    }
+
+    private function increaseViews()
+    {
+        $ids = $this->banners->lists('id');
+        if(count($ids)) Banner::whereIn('id', $ids)->increment('views');
     }
 
 }
